989f252f7302a3f7d16df97a9c0be431e6140d5e
vbonta <vlad.bonta@rodeapps.com>
2017-01-06 16:33:36 +0200

    Last version added

--- Client.scala
+++ Client.scala
@@ -8,7 +8,7 @@ import scala.io._
 import java.util.ArrayList
 import scala.swing.event.SelectionChanged
 
-object Client extends JFrame{
+object Client extends JFrame {
 
   class ClientClass() {
     setTitle("Test Client");
@@ -19,23 +19,29 @@ object Client extends JFrame{
     var in = new BufferedSource(socket.getInputStream).getLines();
     val out = new PrintStream(socket.getOutputStream);
     val userlist = new ArrayList[String];
-    var userArray : Array[String] = Array();
+    var userArray: Array[String] = Array();
     var table = new JList[String]();
+    var socketPeerToPeer = new Socket();
+    var peerConnected = false;
 
     println("Client initialized:");
+    val listenerSocket = 5556;
 
     def startListening() {
       try {
-        val server = new ServerSocket(5556);
+        val server = new ServerSocket(listenerSocket);
         println("Server created");
         val listeningThread = new Thread(new Runnable {
           def run() {
             try {
-
+              peerConnected = true;
+              out.println("SUCCESS/" + listenerSocket);
+              out.flush();
               println("Listening for other client");
               while (true) {
                 val otherClient = server.accept();
-
+                socketPeerToPeer = otherClient;
+                startBRP();
                 println("Other client has connected!");
 
                 val clientThread = new Thread(new Runnable {
@@ -44,9 +50,9 @@ object Client extends JFrame{
                     val out = new PrintStream(otherClient.getOutputStream());
 
                     while (in.ready()) {
-
                       val message = in.readLine();
-                      out.println("Response after accepting connection from you");
+                      println("Message readed from listening user: " + message);
+                      out.println("#listeningUser: Response after accepting connection from you");
                       println(message);
                     }
                   }
@@ -55,7 +61,8 @@ object Client extends JFrame{
                 clientThread.start();
               }
             } catch {
-              case e: Exception => println(e);
+              case e: Exception => out.println("FAILURE");
+                println(e);
             }
           }
         });
@@ -65,18 +72,40 @@ object Client extends JFrame{
         case e: Exception => ;
       }
     }
+    def startBRP(){
 
+      var shouldRunLocal = true;
+        try {
+          while (shouldRunLocal) {
+            var in = new BufferedReader(new InputStreamReader(socketPeerToPeer.getInputStream())).readLine();
+            val messageFromTheOtherClient = in;
+            println("The other user sent:  " + messageFromTheOtherClient);
+            if (messageFromTheOtherClient.equals("DISCONNECT")){
+              println("I should autocancel myself!");
+              shouldRunLocal = false;
+            }
+
+            
+          }
+        } catch {
+          case e: Exception => println("Error errorError errorError errorError error");
+        }
+
+    }
     def connectToPeer(address: String) {
-      val socketPeerToPeer = new Socket(InetAddress.getByName(address), 5556);
-      println("Connected to first client");
+      peerConnected = true;
+      println("SocketAdressToConnect: " + address);
+      socketPeerToPeer = new Socket(InetAddress.getByName("localhost"), Integer.valueOf(address));
+      println("Connected to the other client");
       val outPeerToPeer = new PrintStream(socketPeerToPeer.getOutputStream);
-      outPeerToPeer.println("I have connected to you and this is my message");
+      outPeerToPeer.println("#Requester user: I have connected to you and this is my message");
       var inPeerToPeer = new BufferedSource(socketPeerToPeer.getInputStream).getLines();
-
       System.out.println(inPeerToPeer.next());
+      
+      startBRP();
     }
 
-    startListening();
+    // startListening();
 
     val messsageTextField = new JTextField(20);
     val contents = new JPanel();
@@ -87,7 +116,14 @@ object Client extends JFrame{
 
     sendButton.addActionListener(new ActionListener() {
       def actionPerformed(e: ActionEvent) {
-        println("SEND");
+
+        if (peerConnected){
+          println("SENDING :");
+          println(messsageTextField.getText());
+          
+          val outPeerToPeer = new PrintStream(socketPeerToPeer.getOutputStream);
+          outPeerToPeer.println(messsageTextField.getText());
+        }
       };
     });
 
@@ -109,32 +145,7 @@ object Client extends JFrame{
       def actionPerformed(e: ActionEvent) {
         out.println("REQ_USERLIST");
         out.flush();
-        val receivedList = in.next();
-        val splitList = receivedList.split("\\|");
-        println("List of clients connected to server: ");
 
-        userlist.clear();
-        for ( str <- splitList) {
-          userlist.add(str);
-          println(str);
-        }
-
-        userArray = Array();
-        userArray = userlist.toArray[String](userArray);
-
-        contents.remove(table);
-        table = new JList[String](userArray);
-        table.setMinimumSize(new Dimension(200, 200));
-        table.setPreferredSize(new Dimension(200, 200));
-        table.addListSelectionListener(new ListSelectionListener() {
-          def valueChanged(e: ListSelectionEvent){
-            println(table.getSelectedIndex());
-          }
-        });
-        contents.add(table);
-        contents.repaint();
-        contents.setVisible(false);
-        contents.setVisible(true);
       };
     })
 
@@ -145,6 +156,66 @@ object Client extends JFrame{
     add(contents);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setVisible(true);
+
+    val serverListener = new Thread(new Runnable {
+      def run() {
+        var shouldRunLocal = true;
+        try {
+          while (shouldRunLocal) {
+            var in = new BufferedReader(new InputStreamReader(socket.getInputStream())).readLine();
+            val socketToOpen = in;
+
+            println("Server sent " + socketToOpen);
+
+            if (socketToOpen.startsWith("LISTENSOCKET")) {
+              startListening();
+            } else if (socketToOpen.startsWith("TALKSOCKET")) {
+              connectToPeer(socketToOpen.split("/")(1));
+            } else if (socketToOpen.startsWith("CLIENT")) {
+              val receivedList = in;
+              val splitList = receivedList.split("\\|");
+              var selectedUserListIndex = -1;
+              println("List of clients connected to server: ");
+
+              userlist.clear();
+              for (str <- splitList) {
+                userlist.add(str);
+                println(str);
+              }
+
+              userArray = Array();
+              userArray = userlist.toArray[String](userArray);
+
+              contents.remove(table);
+              table = new JList[String](userArray);
+              table.setMinimumSize(new Dimension(200, 200));
+              table.setPreferredSize(new Dimension(200, 200));
+              table.addListSelectionListener(new ListSelectionListener() {
+                def valueChanged(e: ListSelectionEvent) {
+
+                  if (selectedUserListIndex != table.getSelectedIndex()) {
+                    out.println(userlist.get(0).split("/")(0));
+                    selectedUserListIndex = table.getSelectedIndex();
+                    println("Selected index from table:")
+                    println(table.getSelectedIndex());
+                    val clientToConnect = in;
+                    println("clientToConnectSocket " + clientToConnect);
+
+                  }
+                }
+              });
+              contents.add(table);
+              contents.repaint();
+              contents.setVisible(false);
+              contents.setVisible(true);
+            }
+          }
+        } catch {
+          case e: Exception => println("Error errorError errorError errorError error");
+        }
+      }
+    });
+    serverListener.start();
   }
 
   def main(args: Array[String]) {
--- Server.scala
+++ Server.scala
@@ -7,21 +7,25 @@ object Server {
   var clients = new ArrayList[Socket]();
   var numberOfClients = 0;
   var shouldRun = true;
+  var userRequestingConnectionToAnotherUser = new Socket();
 
 
   val connectionListener = new Thread(new Runnable {
     def run() {
       try {
         val server = new ServerSocket(5555);
+        var numberOfConnectedUsers = 0;
         println("Listening for connections");
+
         while (shouldRun) {
           val currentClient = server.accept();
+
           clients.add(currentClient);
           numberOfClients = numberOfClients + 1;
-
           val requestListener = new Thread(new Runnable {
             def run() {
               var shouldRunLocal = true;
+
               try {
                 while (shouldRunLocal) {
                   var in = new BufferedReader(new InputStreamReader(currentClient.getInputStream())).readLine();
@@ -29,14 +33,17 @@ object Server {
                   if (in == null) {
                     in = "DISCONNECT";
                   }
+
+
                   println("Client " + clients.indexOf(currentClient) + " sent: " + in);
 
-
                   if (in.equals("DISCONNECT")) {
+                    //Client disconnected
                     clients.remove(currentClient);
                     numberOfClients = numberOfClients - 1;
                     shouldRunLocal = false;
                   } else if (in.equals("REQ_USERLIST")) {
+                    //Client requests users list
                     var currentIndex = 0;
                     var clientList = "";
                     for (i <- 0 to clients.size() - 1) {
@@ -45,7 +52,32 @@ object Server {
                       }
                     }
                     out.println(clientList);
+                  } else if (in.startsWith("CLIENT")) {
+                    //Client wants to connect to CLIENT X
+
+                    userRequestingConnectionToAnotherUser = currentClient;
+                    val listeningUserIndex = Integer.valueOf(in.replaceAll("[^0-9]", ""));
+                    numberOfConnectedUsers += 1;
+                    val userToUserSocket = 5555 + numberOfConnectedUsers;
+                    //Send to the receiver user the socket that he should open
+                    val client = clients.get(listeningUserIndex);
+
+                    val receiverUserOut = new PrintStream(client.getOutputStream());
+                    receiverUserOut.println("LISTENSOCKET/" + userToUserSocket);
+
+                  } else if (in.startsWith("SUCCESS")) {
+                    val sendingUserIndex = Integer.valueOf(in.replaceAll("[^0-9]", ""));
+                    val receiverUserOut = new PrintStream(userRequestingConnectionToAnotherUser.getOutputStream());
+
+                    receiverUserOut.println("TALKSOCKET/" + sendingUserIndex);
+
+                  } else if (in.equals("FAILURE")) {
+                    numberOfConnectedUsers -= 1;
+                    val receiverUserOut = new PrintStream(userRequestingConnectionToAnotherUser.getOutputStream());
+                    receiverUserOut.println("RECEIVER_USER_CAN'T_OPEN_SOCKET");
                   }
+
+
                 }
               } catch {
                 case e: Exception => clients.remove(currentClient);
